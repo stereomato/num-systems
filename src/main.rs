828cb57713bfef1b8d042c0737332950637cefb8
Luis <stereomato@proton.me>
2024-09-24 22:55:16 -0500

    finalize!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 
-
-
+use std::io;
 
 fn check_input(input: String, radix: u32) -> bool {
     // can't do this comfortably
@@ -41,7 +40,7 @@ fn check_input(input: String, radix: u32) -> bool {
 }
 
 // function that converts from any system to decimal
-fn to_decimal(input: String, radix: u8) -> String {
+fn to_decimal(input: &String, input_radix: u8) -> String {
     // can use len() due to just handling simple text
     let input_divided: Vec<&str> = input.split_terminator('.').collect();
     let mut counter: isize = input_divided[0].to_string().len() as isize;
@@ -54,13 +53,13 @@ fn to_decimal(input: String, radix: u8) -> String {
         } else {
             counter -= 1;
             output += match character {
-                'A' => 10.0 * (radix as f64).powi(counter as i32),
-                'B' => 11.0 * (radix as f64).powi(counter as i32),
-                'C' => 12.0 * (radix as f64).powi(counter as i32),
-                'D' => 13.0 * (radix as f64).powi(counter as i32),
-                'E' => 14.0 * (radix as f64).powi(counter as i32),
-                'F' => 15.0 * (radix as f64).powi(counter as i32),
-                _ => character.to_string().parse::<f64>().unwrap() * (radix as f64).powi(counter as i32)
+                'A' => 10.0 * (input_radix as f64).powi(counter as i32),
+                'B' => 11.0 * (input_radix as f64).powi(counter as i32),
+                'C' => 12.0 * (input_radix as f64).powi(counter as i32),
+                'D' => 13.0 * (input_radix as f64).powi(counter as i32),
+                'E' => 14.0 * (input_radix as f64).powi(counter as i32),
+                'F' => 15.0 * (input_radix as f64).powi(counter as i32),
+                _ => character.to_string().parse::<f64>().unwrap() * (input_radix as f64).powi(counter as i32)
             }
         }
     }
@@ -69,7 +68,7 @@ fn to_decimal(input: String, radix: u8) -> String {
 
 // converts given input to a num system
 // still doesn't support floating
-fn to_num_system(input: String, radix: u8) -> String {
+fn decimal_to_system(input: &String, output_radix: u8) -> String {
     // can use len() due to just handling simple text
     let input_divided: Vec<&str> = input.split_terminator('.').collect();
     //let mut counter = 0;
@@ -78,7 +77,7 @@ fn to_num_system(input: String, radix: u8) -> String {
     // integer part
     while quotient > 0 {
         // add handling for radix 16
-        output.insert_str(0, (match quotient % (radix as i64) {
+        output.insert_str(0, (match quotient % (output_radix as i64) {
             10 => 'A',
             11 => 'B',
             12 => 'C',
@@ -87,7 +86,7 @@ fn to_num_system(input: String, radix: u8) -> String {
             15 => 'F',
             x=> char::from_digit(x as u32, 10).unwrap()
         }).to_string().as_str() );
-        quotient /= radix as i64;
+        quotient /= output_radix as i64;
     }
     
     // floating part
@@ -97,7 +96,7 @@ fn to_num_system(input: String, radix: u8) -> String {
         let mut output_floating = "".to_string();
         // convert floating_part to number
         loop {
-            floating_part = (floating_part.parse::<f64>().unwrap() * (radix as f64)).to_string();
+            floating_part = (floating_part.parse::<f64>().unwrap() * (output_radix as f64)).to_string();
             floating_divided = floating_part.split_terminator('.').collect();
             output_floating.push_str(floating_divided[0]);
             
@@ -116,25 +115,67 @@ fn to_num_system(input: String, radix: u8) -> String {
     return output;
 }
 
-fn system_to_system(input: String, input_radix: u32, output_radix: u32) -> String{
-    let mid = to_decimal(input, input_radix as u8);
-    return to_num_system(mid, output_radix as u8);
+fn system_to_system(input: &String, input_radix: u8, output_radix: u8) -> String{
+    let mid = to_decimal(&input, input_radix);
+    return decimal_to_system(&mid, output_radix);
 }
 
 fn main() {
-    let  yo = "153";
+    let mut choice = "".to_string();
+    let mut input = "".to_string();
+    let mut input_radix = 0;
+    let mut output_radix = 0;
+    let mut output = "".to_string();
 
-    println!("{}", '.' as u32);
-    println!("{}", yo);
-    println!("{}", system_to_system(yo.to_string(), 8, 2));
-    //println!("{}", to_num_system(yo.to_string(), 8));
-
-    //println!("{}", to_decimal(yo.to_string(), 16));
-    // have to check over here if the given input is proper
-    if check_input(yo.to_string(),16) {
-        println!("wowee");
-    } else {
-        println!("fuck");
+    println!("Bienvenido, ¿De cual base es el número que desea convertir?");
+    println!("1. Decimal, 2. Binario, 3. Octal, 4. Hexadecimal.");
+    println!("Ingrese su elección en la siguiente línea.");
+    io::stdin().read_line(&mut choice).expect("De alguna manera algo falló");
+    choice = choice.trim().to_string();
+    match choice.as_str() {
+        "1" => input_radix = 10,
+        "2" => input_radix = 2,
+        "3" => input_radix = 8,
+        "4" => input_radix = 16,
+        _ => {
+            println!("Valor inesperado.");
+        }
     }
+
+    println!("¿Cuál es el número que desea convertir?");
+    println!("Ingrese el número en la siguiente línea.");
+    io::stdin().read_line(&mut input).expect("De alguna manera algo falló");
+    input = input.trim().to_string();
     
+    println!("Bien, ¿a qué sistema desea convertir el número?");
+    println!("1. Decimal, 2. Binario, 3. Octal, 4. Hexadecimal.");
+    println!("Ingrese su elección en la siguiente línea.");
+    choice = "".to_string();
+    io::stdin().read_line(&mut choice).expect("De alguna manera algo falló");
+    choice = choice.trim().to_string();
+
+    match choice.as_str() {
+        "1" => output_radix = 10,
+        "2" => output_radix = 2,
+        "3" => output_radix = 8,
+        "4" => output_radix = 16,
+        _ => {
+            println!("Valor inesperado.");
+        }
+    }
+
+    match output_radix {
+        10 => output = to_decimal(&input, input_radix),
+        2 | 8 | 16 => {
+            if input_radix != 10 {
+                output = system_to_system(&input, input_radix, output_radix);
+            } else {
+                output = decimal_to_system(&input, output_radix);
+            }
+        }
+        _ => println!("Error inesperado"),
+    }
+
+    println!("El número de entrada fue: {}", input);
+    println!("El número de salida, convertido a base {}, fue: {}", output_radix, output);
 }
